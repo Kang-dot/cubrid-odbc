201c9c36f61ef632c1443cb0a003876b3b920ce4
Kang-dot <jgbu97@gmail.com>
2025-01-21 20:14:47 +0900

    [APIS-1007] edit unit test cpp project
    - edit unit test cpp project for x64 test
    - add file output function in test batch file

--- test.bat
+++ test.bat
@@ -1,14 +1,23 @@
 set BATCH_HOME=%~dp0
-set VSTestTool=%VS2017COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
+set VSTESTTOOL=%VS2017COMNTOOLS%..\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 call "%VS2017COMNTOOLS%VsDevCmd.bat"
 cd UnitTest
-devenv UnitTest_14.sln /Upgrade
 devenv UnitTest_14.sln /Project UnitTest\UnitTest.csproj /Rebuild "Release|x64" 
 cd UnitTest\bin\x64\Release
-call "%VSTestTool%" UnitTest.dll --platform:x64
-
-cd %BATCH_HOME%\UnitTest-CPP
-devenv UnitTest_14.sln /Upgrade
+ 
+if "%1"=="-p" (
+  powershell -Command "& '%VSTESTTOOL%' UnitTest.dll --platform:x64 | Tee-Object -FilePath '%BATCH_HOME%..\unit_test.result'"
+) else (
+  call "%VSTESTTOOL%" UnitTest.dll --platform:x64
+)
+ 
+cd %BATCH_HOME%UnitTest-CPP
+devenv UnitTest-CPP.sln /Upgrade
 devenv UnitTest-CPP.sln /Project UnitTest-CPP\UnitTest-CPP.vcxproj /Rebuild "Release|x64" 
 cd x64\Release
-call "%VSTestTool%" UnitTest-CPP.dll --platform:x64
+ 
+if "%1"=="-p" (
+  powershell -Command "& '%VSTESTTOOL%' UnitTest-CPP.dll --platform:x64 | Tee-Object -FilePath '%BATCH_HOME%..\unit_test_cpp.result'"
+) else (
+  call "%VSTESTTOOL%" UnitTest-CPP.dll --platform:x64
+)
